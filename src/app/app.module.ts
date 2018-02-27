e08b264013f6a33bfb9d8ec92f90f121bd46961c
lucas2500 <lucasrafael654@gmail.com>
2018-02-27 14:52:32 -0300

    <Primeiro commit neste repo>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,25 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import { HttpModule } from '@angular/http';
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    HttpModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController} from 'ionic-angular';
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+	selector: 'page-home',
+	templateUrl: 'home.html',
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+	private url: string = 'https://beer.symfonycasts.com.br/v1';
+	public index: Array<{}>;
 
-  }
+	constructor(public http: Http, public navCtrl: NavController) { 
+
+		this.http.get(this.url + '/beers')
+		.map(res => res.json())
+		.subscribe(data => {
+
+			console.log(data);
+
+		});
+
+	}
+	// listarFunc() {
+
+	// 	this.navCtrl.push(FuncionariosPage);
+
+	// }
 
 }
